4ecaae2a975bb9a52a39a022f4e68b89249f5163
Lautaro Aliquó <lautaro.aliquo@gmail.com>
2025-09-03 15:17:52 -0300

    Corrigiendo ejericio 2

--- TP4_prog/ejercicio2.py
+++ TP4_prog/ejercicio2.py
@@ -1,6 +1,16 @@
-numero: str = input("Ingrese un número entero: ")
 digitos: int = 0
-for i in numero:
-    digitos += 1
+# while numero[0] not in "123456789":
+#     numero = numero[1: -1]
+bandera: bool = True
 
-print(f"El número ingresado tiene {digitos} dígitos.")
+while bandera:
+    numero: str = input("Ingrese un número entero: ")
+    if not numero.isalpha():
+        bandera = False
+    else:
+        bandera = True
+for i in numero:
+    if i != 0:
+        digitos += 1
+
+print(f"El número ingresado tiene {digitos} dígito(s).")
