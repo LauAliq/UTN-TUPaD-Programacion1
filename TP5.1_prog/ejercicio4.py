b361e2ca017388532e119e6173cade39cdb47cca
Lautaro Aliquó <lautaro.aliquo@gmail.com>
2025-09-23 15:46:27 -0300

    Cuarto ejercicio

---
+++ TP5.1_prog/ejercicio4.py
@@ -0,0 +1,18 @@
+# Creamos la lista con posibles elementos repetidos
+
+datos = [1, 3, 5, 3, 7, 1, 9, 5, 3]
+
+# Creamos la lista que almacenará los elementos de la lista datos sin repetir
+
+datos_sin_repetir = []
+
+# Rellenamos la lista datos_sin_repetir con los elementos de la lista datos sin repetir
+
+for i in range(len(datos)):
+    if datos[i] not in datos_sin_repetir:
+        datos_sin_repetir.append(datos[i])
+
+# Imprimimos ambas listas
+
+print(f"La lista con posibles elementos repetidos es {datos}.")    
+print(f"La lista sin elementos repetidos es {datos_sin_repetir}.")
